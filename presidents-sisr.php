72690a834727028a28223a5611b5a2a700d965a9
bpesquet <bpesquet@gmail.com>
2016-04-06 11:29:29 +0200

    Exercice 1

---
+++ presidents-sisr.php
@@ -0,0 +1,18 @@
+<?php
+$presidents = array("De Gaulle", "Pompidou", "Giscard d'Estaing", "Mitterrand",
+    "Chirac", "Sarkozy", "Hollande");
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Les Présidents</title>
+</head>
+    <h1>Les Présidents de la Vème République</h1>
+    <?php
+    foreach($presidents as $president) {
+        echo $president . "<br>";
+    }
+    ?>
+</html>
---
+++ presidents-slam.php
@@ -0,0 +1,22 @@
+<?php
+require 'fonctions.php';
+$presidents = array("De Gaulle", "Pompidou", "Giscard d'Estaing", "Mitterrand",
+    "Chirac", "Sarkozy", "Hollande");
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <title>Les Présidents</title>
+</head>
+    <h1>Les Présidents de la Vème République</h1>
+    <ul>
+    <?php
+    foreach($presidents as $president) {
+        echo "<li>" . escape($president) . "</li>";
+    }
+    ?>
+    </ul>
+</html>
